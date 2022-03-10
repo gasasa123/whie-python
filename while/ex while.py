4e8627cb2a26e5be747a34ba10b5facda37968d6
rivka123 <gasasa255@gmail.com>
2022-03-10 16:49:39 +0200

    while ex

--- while/ex while.py
+++ while/ex while.py
@@ -1,11 +1,25 @@
 # 1
- x = 0
- while x < 20:
-     x += 1
-     if x % 2 != 0:
-         print(x)
+#  x = 0
+#  while x < 20:
+#      x += 1
+#      if x % 2 != 0:
+#          print(x)
 
 # 2
+guessnum=int(input("please choose numbetween 1 -1000:"))
+start=1
+end=1000
+middle =(start+end)//2
+print("start=",start,"middle=",middle,"end=",end,"number=",guessnum)
+while guessnum!=middle:
+    middle = (start+end)//2
+    if guessnum>middle:
+        start=middle
+        print("start=",start,"end=",end,"number=",guessnum)
+    else:
+        end=middle
+        print("start=",start,"end=",end,"number=",guessnum)
+print("found",guessnum)
 
 
 
